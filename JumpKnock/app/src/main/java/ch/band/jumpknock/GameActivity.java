68cd9b23518e5fdfce2a8acd5ba7910098dce11e
MankoMR <manuel.koloska@gmail.com>
2020-03-18 16:46:56 +0100

    fix 2 bugs
    *fix player didn't go from the left to the right side of screen after he went out.
    *fix player changing height relative to the screen: with enough time the player wandered ofscreen

--- JumpKnock/app/src/main/java/ch/band/jumpknock/GameActivity.java
+++ JumpKnock/app/src/main/java/ch/band/jumpknock/GameActivity.java
@@ -17,6 +17,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
