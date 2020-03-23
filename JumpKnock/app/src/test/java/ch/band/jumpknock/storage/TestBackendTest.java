624be1e9b6bf051bab0a880a8f100ecafe1bb2f1
FFST <fredystalder.fs@gmail.com>
2020-03-23 10:29:10 +0100

    Erste Testklasse erstellt
    Erste Testklasse erstellt

---
+++ JumpKnock/app/src/test/java/ch/band/jumpknock/storage/TestBackendTest.java
@@ -0,0 +1,37 @@
+package ch.band.jumpknock.storage;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class TestBackendTest {
+
+    private ArrayList<Record> records = new ArrayList<>();
+    //TODO make test
+    //TODO korrigiere name
+    //TODO dokumentation
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void addRecord() {
+    }
+
+    @Test
+    public void removeRecord() {
+    }
+
+    @Test
+    public void getRecords() {
+    }
+}
