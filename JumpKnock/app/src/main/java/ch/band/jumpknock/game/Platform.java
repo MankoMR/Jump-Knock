47b3cd1f5d0150e9a4322f38bd28e24213a08e8f
MankoMR <manuel.koloska@gmail.com>
2020-04-21 18:07:49 +0200

    add further documentation

--- JumpKnock/app/src/main/java/ch/band/jumpknock/game/Platform.java
+++ JumpKnock/app/src/main/java/ch/band/jumpknock/game/Platform.java
@@ -1,6 +1,7 @@
 package ch.band.jumpknock.game;
 
 import androidx.annotation.DrawableRes;
+import androidx.annotation.Nullable;
 
 import ch.band.jumpknock.R;
 /*
@@ -11,32 +12,29 @@ public class Platform extends Placeable {
 	private Decoration decoration = null;
 
 	/**
-	 * gibt den wert aus isOneTimeUse zurück
-	 * @return
+	 * @return whether this platform can be jumped on one time or multiple times.
 	 */
 	public boolean isOneTimeUse() {
 		return isOneTimeUse;
 	}
 
 	/**
-	 * speichert den wert in isOneTimeUse
-	 * @param oneTimeUse
+	 * @param oneTimeUse sets whether this platform can be jumped on one time or multiple times.
 	 */
 	public void setOneTimeUse(boolean oneTimeUse) {
 		isOneTimeUse = oneTimeUse;
 	}
 
 	/**
-	 * gibt den wert aus decoration zurück
-	 * @return
+	 * @return the decoration placed on the platform.
+	 *
 	 */
-	public Decoration getDecoration() {
+	public @Nullable Decoration getDecoration() {
 		return decoration;
 	}
 
 	/**
-	 * speichert den wert in decoration
-	 * @param decoration
+	 * @param decoration to place on the platform
 	 */
 	public void setDecoration(Decoration decoration) {
 		this.decoration = decoration;
